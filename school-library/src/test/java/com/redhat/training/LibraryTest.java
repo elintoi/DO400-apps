fb2ddd6383c846e23b309399fd59b52e74f5d2f8
Elin <elin.toijer@gmail.com>
2023-01-26 06:06:40 -0500

    Refactor

--- school-library/src/test/java/com/redhat/training/LibraryTest.java
+++ school-library/src/test/java/com/redhat/training/LibraryTest.java
@@ -23,5 +23,38 @@ public class LibraryTest {
         library = new Library(inventory);
     }
 
-    // Add tests here...
+    @Test
+    public void checkingOutDecreasesNumberOfBookCopiesFromInventory()
+        throws BookNotAvailableException{
+            // Given
+            inventory.add(new Book("book1"));
+            inventory.add(new Book("book1"));
+     
+            // When 
+            library.checkOut("someStudentId" ,"book1");
+
+            // Then 
+            assertEquals(1, inventory.countCopies("book1"));
+        }   
+
+    @Test
+    public void checkingOutUnavailableBookThrowsException()
+        throws BookNotAvailableException{
+            // Given
+            inventory.add(new Book ("book1"));
+            inventory.add(new Book ("book1"));
+
+            inventory.checkOut("student1", "book1");
+            inventory.checkOut("student2", "book1")
+
+            // When
+            final BookNotAvailableException exception = assertThrows(
+                BookNotAvailableException.class, 
+                () -> {
+                    library.checkOut("student3", "book1");
+                }
+            );
+            // Then 
+            assertTrue(exception.getMessage().matches("Book bok1 is not available"));
+        }
 }
